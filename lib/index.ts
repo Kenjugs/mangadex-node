6dedd8fc4f1892db410c6154df3cb509ae523805
kenjugs <ramarkrusher@gmail.com>
2022-08-14 23:38:47 -0700

    moved index.ts to lib

---
+++ lib/index.ts
@@ -0,0 +1,23 @@
+import * as account from './account';
+import * as auth from './authentication';
+import * as author from './author';
+import * as chapter from './chapter';
+import * as common from './static';
+import * as cover from './cover';
+import * as group from './group';
+import * as list from './list';
+import * as manga from './manga';
+import * as user from './user';
+
+export default {
+    account: account,
+    auth: auth,
+    author: author,
+    chapter: chapter,
+    common: common,
+    cover: cover,
+    group: group,
+    list: list,
+    manga: manga,
+    user: user,
+};
